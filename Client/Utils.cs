9f892ec820d1990c42033c95b63fa1b63838cb3a
Liam <Liam.Hurley2@students.ittralee.ie>
2022-10-04 21:52:33 +0100

    Removed redundant pages. Added logic to only show sort buttons on stats that users should be able to sort by.

---
+++ Client/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace Client
+{
+    public static class Utils
+    {
+        private static readonly List<string> _notSortableStats = new List<string> {
+            "Name", "Club", 
+        };
+
+        public static bool IsASortableStat(string stat) => !_notSortableStats.Contains(stat);
+    }
+}
