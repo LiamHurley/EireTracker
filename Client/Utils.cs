959decb9ea188f3c1b6ba547b78341bcb0d89e1e
Liam <Liam.Hurley2@students.ittralee.ie>
2022-10-07 22:20:50 +0100

    Moved table headers to a static class in utils. Added dropdown list for stats filtering and removed buttons.

--- Client/Utils.cs
+++ Client/Utils.cs
@@ -7,5 +7,15 @@
         };
 
         public static bool IsASortableStat(string stat) => !_notSortableStats.Contains(stat);
+
+        public static class TableHeaders
+        {
+            public static readonly string[] _shootingStatsTableHeaders = { "Name", "Club", "Minutes", "Goals", "Minutes Per Goal", "Shots", "Shots On Target", "Big Chances Missed", };
+            public static readonly string[] _goalkeepingStatsTableHeaders = { "Name", "Club", "Appearances", "Clean Sheets", "Saves",
+                                                                "Pass Completion (%)", "Long Ball Completion (%)", "Errors Leading To Shot", "Average Rating" };
+            public static readonly string[] _defendingStatsTableHeaders = { "Name", "Club", "Appearances", "Minutes", "Duels Won (%)", "Aerial Duels Won (%)",
+                                                              "Tackles", "Shots Blocked", "Errors Leading To Shot" };
+            public static readonly string[] _generalStatsTableHeaders = { "Name", "Club", "Age", "Appearances", "Minutes", "Goals", "Assists", "Average Rating" };
+        }
     }
 }
