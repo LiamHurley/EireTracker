8f300efa4167bff9177ba64c674dd3d42768aee8
Liam <Liam.Hurley2@students.ittralee.ie>
2022-10-08 15:50:37 +0100

    Added Misc + Passing tables

--- Client/Utils.cs
+++ Client/Utils.cs
@@ -10,12 +10,24 @@
 
         public static class TableHeaders
         {
-            public static readonly string[] _shootingStatsTableHeaders = { "Name", "Club", "Minutes", "Goals", "Minutes Per Goal", "Shots", "Shots On Target", "Big Chances Missed", };
+            public static readonly string[] _shootingStatsTableHeaders = { "Name", "Club", "Minutes", "Goals", "Minutes Per Goal", "Shots", 
+                                                                            "Shots On Target", "Big Chances Missed", };
+
             public static readonly string[] _goalkeepingStatsTableHeaders = { "Name", "Club", "Appearances", "Clean Sheets", "Saves",
-                                                                "Pass Completion (%)", "Long Ball Completion (%)", "Errors Leading To Shot", "Average Rating" };
-            public static readonly string[] _defendingStatsTableHeaders = { "Name", "Club", "Appearances", "Minutes", "Duels Won (%)", "Aerial Duels Won (%)",
-                                                              "Tackles", "Shots Blocked", "Errors Leading To Shot" };
-            public static readonly string[] _generalStatsTableHeaders = { "Name", "Club", "Age", "Appearances", "Minutes", "Goals", "Assists", "Average Rating" };
+                                                                            "Pass Completion (%)", "Long Ball Completion (%)", 
+                                                                            "Errors Leading To Shot", "Average Rating" };
+
+            public static readonly string[] _defendingStatsTableHeaders = { "Name", "Club", "Appearances", "Minutes", "Duels Won (%)", 
+                                                                            "Aerial Duels Won (%)", "Tackles", "Shots Blocked", "Errors Leading To Shot" };
+
+            public static readonly string[] _generalStatsTableHeaders = { "Name", "Club", "Age", "Appearances", "Minutes", "Goals", "Assists", 
+                                                                          "Average Rating" };
+
+            public static readonly string[] _passingStatsTableHeaders = { "Name", "Club", "Appearances", "Minutes", "Assists", "Passes", 
+                                                                          "Pass Completion (%)", "Key Passes", "Long Ball Completion (%)" };
+
+            public static readonly string[] _miscStatsTableHeaders = { "Name", "Club", "Appearances", "Minutes", "Touches", "Captain", "Offsides",
+                                                                       "Fouls", "Fouls Won" };
         }
     }
 }
